7055d7cf16cf1b7740476f9fa5ddcf86d5b65f92
Victor-Arum <152734553+Victor-Arum@users.noreply.github.com>
2025-01-08 13:40:40 +0000

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,110 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+
+def load_dataset(file_path):
+    with open(file_path, "r") as file:
+        dataset = json.load(file)
+
+    X = [] # Input messages
+    y = [] # Intent labels
+
+    for intent, data in dataset.items():
+        for pattern in data["patterns"]:
+            X.append(pattern.lower()) # Convert patterns to lowercase
+            y.append(intent)
+
+    return X, y, dataset
+
+dataset_file = "adidas_chatbot.json"
+X, y, dataset = load_dataset(dataset_file)
+intents = y
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(intent, dataset):
+    responses = dataset[intent]["responses"]
+    return np.random.choice(responses)
+
+def chatbot_response(text):
+    ints = predict_class(text, model)
+    tag = ints[0]['intent']
+    response = get_response(tag, dataset)
+    return response
+
+import tkinter
+from tkinter import *
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+base = Tk()
+base.title("Adidas Help")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.config(state=DISABLED)
+
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
